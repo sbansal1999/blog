829fec1243cc5dfad119f5155fc896a5e344a5e6
Satyam Bansal <sbansal1999@gmail.com>
2023-05-29 23:10:13 +0530

    add some stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://sbansal1999.github.io",
-  base: "/",
   integrations: [tailwind()]
 });
