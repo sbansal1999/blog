2fa75f3b5608dbac594127fe2992980ad544c592
Satyam Bansal <sbansal1999@gmail.com>
2023-05-29 18:51:36 +0530

    add license

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://sbansal1999.github.io",
+  base: "/my-repo",
+});
